5920d21c6a8f9f89eb3bac53b476486b548aaaee
Carlos Garcia <qlucag1@paceind.com>
2023-07-12 16:25:57 -0600

    A file

---
+++ lib/pages/logsMovePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LogsMovement extends StatefulWidget {
+  const LogsMovement({super.key});
+
+  @override
+  State<LogsMovement> createState() => _LogsMovementState();
+}
+
+class _LogsMovementState extends State<LogsMovement> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("");
+  }
+}
